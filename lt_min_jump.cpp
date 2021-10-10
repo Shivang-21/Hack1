0974d10d3c2d9da5b2646a5663007d1429261ec6
GitIt8081 <57960303+GitIt8081@users.noreply.github.com>
2021-10-11 02:12:28 +0530

    Update lt_min_jump.cpp

--- lt_min_jump.cpp
+++ lt_min_jump.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> dp(n,20000000);
-        if(n==1)
-            return 0;
-        dp[n-1]=0;
-        dp[n-2]=1;
-        for(int i=n-3;i>=0;i--)
+        int n=nums.size(),m=0,c=0,prev=0;
+        for(int i=0;i<n-1;i++)
         {
-            int range=nums[i]+i<=n-1?nums[i]+i:n-1;
-            for(int j=i+1;j<=range;j++)
+            if(m<i+nums[i])
+                m=i+nums[i];
+            if(i==prev)
             {
-                dp[i]=min(dp[i],dp[j]+1);
+                c++;
+                prev=m;
             }
         }
-        return dp[0];
+        return c;
     }
 };
